2c503b39a6eff5e248ea8503dc1314a333a64f8f
webmisfit <76257594+webmisfit@users.noreply.github.com>
2022-10-17 20:30:37 +0600

    count-cats

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
+function getCommonCharacterCount(s1, s2) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let x = s1 + s2
+
+  let y = new Set(x)
+
+  // const reducer = (a,c) => 
+
+  // let z = 
+
+
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  // console.log(matrix)
+
+  return matrix.flat().reduce((a,c)=>{
+    a += +(c=='^^')
+
+    return a
+  },0)
+
 }
 
 module.exports = {
